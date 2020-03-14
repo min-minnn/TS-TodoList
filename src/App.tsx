099dd024129b30f0ae0ab94b59b31bf7e6f25e4e
min-minnn <alswlk0306@gmail.com>
2020-03-14 19:50:40 +0900

    Add style

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import TodoList from './components/TodoList';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  width: 30rem;
+  border-radius: 0.25rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  margin-top: 4rem;
+  box-shadow: 0rem 0rem 1.25rem #d7d7d7;
+  font-family: 'Roboto';
+  font-size: 1rem;
+`
 
 const App: React.FC = () => {
-  /*const onCreate = (text: string): void => {
-    const todo = {
-      id: todos.length ? todos[todos.length - 1].id + 1 : 1,
-      text,
-      done: false,
-    };
-
-    setTodos(todos => todos.concat(todo));
-  };*/
-  
   return (
-    <>
+    <Background>
       <TodoList />
-    </>
+    </Background>
   );
 }
 
